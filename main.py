178b1965ee78fc391663863b5b4a14e7306ec58e
Jakub Hekal <jakub.hekal@gmail.com>
2024-05-22 21:32:18 +0200

    main.py cleaned up

--- main.py
+++ main.py
@@ -9,17 +9,16 @@ from sensor_device import *
 from sftp_connection import SFTPConnection
 
 
-def record(microphone):
-    device = RecordingDevice.get_device_by_name(microphone['name'], microphone['index'])
+def record(hive_id, microphone):
     path = os.path.join(
         os.path.dirname(__file__),
-        f'{config['general']['station']}-{microphone['hive']}-{int(time.time())}.wav'
+        f'{hive_id}-{int(time.time())}.wav'
     )
-    device.record(path, config['general']['recording']['duration'])
+    microphone.record(path, config['general']['recording']['duration'])
 
     remote_path = sftp.upload_recording(path)
     if sftp.file_exists(remote_path):
-        db.insert_sound_data(device.device_index, remote_path)
+        db.insert_sound_data(hive_id, remote_path)
 
 
 def measure(hive_id, sensors):
@@ -52,17 +51,22 @@ if __name__ == '__main__':
     )
 
     hive_sensors = {}
-    for sensor in config['sensors']:
-        sensor_device = SensorDevice.setup_from_config(sensor)
-        for hive in sensor['hive']:
+    for sensor_config in config['sensors']:
+        sensor_device = SensorDevice.setup_from_config(sensor_config)
+        for hive in sensor_config['hive']:
+            hive = f'{config['general']['station']}-{hive}'
             if hive not in hive_sensors:
                 hive_sensors[hive] = []
             hive_sensors[hive].append(sensor_device)
 
+    hive_microphone = {}
+    for microphone_config in config['microphones']:
+        recording_device = RecordingDevice.get_device_by_name(microphone_config['name'], microphone_config['index'])
+        hive_microphone[f'{config['general']['station']}-{microphone_config['hive']}'] = recording_device
+
     with concurrent.futures.ThreadPoolExecutor(max_workers=5) as executor:
-        for m in config['microphones']:
-            print(m)
-            schedule.every(config['general']['recording']['interval']).seconds.do(executor.submit, record, m)
+        for h, m in hive_microphone.items():
+            schedule.every(config['general']['recording']['interval']).seconds.do(executor.submit, record, h, m)
         for h, s in hive_sensors.items():
             schedule.every(config['general']['measurements']['interval']).seconds.do(executor.submit, measure, h, s)
         while 1:
